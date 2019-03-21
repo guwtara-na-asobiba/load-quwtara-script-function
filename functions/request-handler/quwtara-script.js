e6d6ae0d71120b187db8a6610cae9e468533c6e6
みゃみゅ玉子 <myamyu@gmail.com>
2019-03-21 21:36:14 +0900

    バケット名をconfigから取るようにした。ついでにリファクタリング等。 (#5)

--- functions/request-handler/quwtara-script.js
+++ functions/request-handler/quwtara-script.js
@@ -3,8 +3,13 @@ const express = require('express');
 const admin = require('firebase-admin');
 const app = express();
 const storage = admin.storage();
+const config = functions.config().scripts || {
+  bucket: {
+    name: storage.app.options.storageBucket,
+  },
+};
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
   res.sendStatus(404);
 });
 
@@ -13,24 +18,25 @@ app.get('/', (req, res) => {
  */
 app.get('/:name', (req, res) => {
   const scriptName = req.params.name;
-  const bucket = storage.bucket();
+  const bucket = storage.bucket(config.bucket.name);
   const file = bucket.file(`scripts/${scriptName}/index.js`);
+  const errHandle = (err) => {
+    console.error('エラーが発生', err);
+    res.sendStatus(500);
+  };
   file.exists()
       .then((exists) => {
-        if (!exists) {
+        if (!exists[0]) {
           res.sendStatus(404);
-          return;
+          throw new Error(`「${scriptName}」なんてないよ`);
         }
         return file.download();
-      })
+      }, errHandle)
       .then((buf) => {
         res.set('Cache-Control', `public, max-age=${12*60}, s-maxage=${24*60}`);
         res.set('Content-Type', 'application/javascript; charset=utf-8');
         res.status(200).send(buf.toString('utf-8', 0, buf.length));
-      }, (err) => {
-        console.error('エラーが発生', err);
-        res.sendStatus(500);
-      });
+      }, errHandle);
 });
 
 module.exports = functions.https.onRequest(app);
